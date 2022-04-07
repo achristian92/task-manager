f14cd11e683dafcec7ea07512436f0fb37c3c16f
Alan Ruiz Aguirre <aruiz@Alans-MacBook-Pro.local>
2022-04-07 15:11:57 -0500

    test2 deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,8 +32,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app.env') === 'production')
-            URL::forceScheme('https');
+//        if(config('app.env') === 'production')
+//            URL::forceScheme('https');
 
         Carbon::setLocale(config('app.locale'));
 
