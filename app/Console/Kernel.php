b1f00657e2313e3c4fb6f0e673564fafe7bd11ad
Alan Christian Ruiz Aguirre <christian922017@gmail.com>
2024-10-22 15:07:15 -0500

    kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends ConsoleKernel
             ->emailOutputOnFailure('alan.ruiz@brainbox.pe');
 
         $schedule->command('limithours:customer')
-            ->dailyAt('08:00')
+            ->dailyAt('15:15')
             ->emailOutputOnFailure('alan.ruiz@brainbox.pe');
     }
 
